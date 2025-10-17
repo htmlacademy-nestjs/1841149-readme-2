3a9406991a52670721fb9eeb96c7449a6226dd22
Лисица Григорий <lisitsa-gp@ranepa.ru>
2025-10-17 15:04:44 +0300

    module7-task

--- project/apps/api-gateway/src/main.ts
+++ project/apps/api-gateway/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { RequestIdInterceptor } from './app/interceptors/request-id.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const GLOBAL_PREFIX = 'api';
@@ -9,11 +10,24 @@ async function bootstrap() {
   const port = process.env.PORT || DEFAULT_PORT;
 
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The "API-Gateway" service')
+    .setDescription('API-Gateway service API')
+    .setVersion('1.0')
+    .build();
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/spec', app, document);
+
   app.setGlobalPrefix(GLOBAL_PREFIX);
   app.useGlobalInterceptors(new RequestIdInterceptor());
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
+  );
+  Logger.log(
+    `📚 Swagger UI is available at: http://localhost:${port}/${GLOBAL_PREFIX}/spec`
   );
 }
 
